9723a1f2d37eaf1ef5c8d66ea0ef049a9eafee08
Sean Kim <33474168+seankim658@users.noreply.github.com>
2023-08-31 09:38:46 -0400

    continued testing

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import os 
 
 class DataExtractionEngine: 
     ''' Class for the main data extraction engine. 
@@ -7,14 +8,21 @@ class DataExtractionEngine:
     ----------------
     column_names : List[str]
         Column names for the output Dataframe. 
+    file_types : Set{str}
+        Set of valid file input types. 
 
     Instance Attributes
     -------------------
+    raw_data : pd.DataFrame
+        DataFrame containing raw data loaded in from input file. 
 
     Methods
     -------
     __init__(input_file, input_type)
         Constructor. 
+    
+    read_in_data(input_file, input_type) 
+        Reads in the data from the user inputted file path and returns it as a Pandas DataFrame. 
     '''
 
     column_names = ['biomarker_id', 'main_x_ref', 'assessed_biomarker_entity', 'biomarker_status',
@@ -36,6 +44,24 @@ class DataExtractionEngine:
         # make sure inputs are valid 
         if input_type not in self.file_types:
             raise ValueError('Input file type invalid.')
+        if not os.path.isfile(f'./{input_file}'):
+            raise ValueError(f'Input file does not exist. Recheck inputted file path/name.')
+        
+        # load in data 
+        self.raw_data = self.read_in_data(input_file, input_type)
+    
+    def read_in_data(self, input_file: str, input_type: str) -> pd.DataFrame:
+        ''' Reads in the data from the user inputted file path and returns a Pandas Dataframe. 
 
+        Parameters
+        ----------
+        input_file : str
+            File path to the input file (pre-validated in contructor).
+        input_type : str 
+            File type of the input fiel (pre-validated in constructor).
+        '''
+
+        return pd.read_csv(input_file)
+    
 
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from data_extraction import DataExtractionEngine
+import os 
 
 INPUT_FILE = './data/clinvar.csv'
 INPUT_TYPE = 'csv'
-extractor = DataExtractionEngine(INPUT_FILE, INPUT_TYPE)
+# extractor = DataExtractionEngine(INPUT_FILE, INPUT_TYPE)
