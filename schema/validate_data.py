b4135d7e13373ace75453223ad604ab228481082
skim658 <33474168+seankim658@users.noreply.github.com>
2023-10-07 13:03:33 -0400

    init

---
+++ schema/validate_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/python3
+''' BiomarkerKB data validator. Validates a given input set against the data dictionary JSON schema. 
+
+Usage: python validate_data.py [options]
+
+    Positional arguments:
+        data_filepath       filepath of the input data file to validate
+        schema_filepath     filepath to the schema file to validate against
+    
+    Optional arguments
+        -h --help           show the help message and exit
+        -v --version        show current version number and exit 
+'''
