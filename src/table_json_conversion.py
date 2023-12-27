4413df10ba82d33acd0b8f070805b3fa03049aa8
Sean Kim <33474168+seankim658@users.noreply.github.com>
2023-12-27 14:06:37 -0500

    ready to test script

--- src/table_json_conversion.py
+++ src/table_json_conversion.py
@@ -33,6 +33,8 @@ _version = None
 TSV_HEADERS = ['biomarker_id', 'biomarker', 'assessed_biomarker_entity', 'assessed_biomarker_entity_id', 
             'assessed_entity_type', 'condition', 'condition_id', 'exposure_agent', 'exposure_agent_id', 
             'best_biomarker_role', 'specimen', 'specimen_id', 'loinc_code', 'evidence_source', 'evidence', 'tag']
+URL_MAP = None
+NAME_SPACE_MAP = None
 
 def validate_filepath(filepath: str, mode: str) -> None:
     ''' Validates the filepaths for the user inputted source path and
@@ -224,13 +226,13 @@ def json_to_tsv(source: str, target: str) -> None:
                             for evidence_text in evidence_source['evidence_list']:
                                 evidence_values.append(evidence_text['evidence'])
                             # add evidence to set for reference in top level evidence handling
-                            seen_evidence.add(' '.join(evidence_values))
+                            seen_evidence.add('; '.join(evidence_values))
                         
                         # reset flag for next evidence entry 
                         add_comp_evidence_values_flag = False
                             
                         # populate evidence columns and add to current row
-                        evidence_columns = [evidence_source_value, ' '.join(evidence_values), '; '.join(tag_values)]
+                        evidence_columns = [evidence_source_value, '; '.join(evidence_values), '; '.join(tag_values)]
                         tsv_content += '\t'.join(row_data) + '\t' + '\t'.join(evidence_columns) + '\n'
                     
                     # re-initialize evidence column values to empty strings 
@@ -260,14 +262,14 @@ def json_to_tsv(source: str, target: str) -> None:
                             for evidence_text in evidence_source['evidence_list']:
                                 evidence_values.append(evidence_text['evidence'])
                             # check if evidence is already captured from component evidence
-                            if ' '.join(evidence_values) in seen_evidence:
+                            if '; '.join(evidence_values) in seen_evidence:
                                 continue
                         
                         # reset flag for next evidence entry
                         add_top_level_evidence_values_flag = False 
 
                         # populate evidence columns and add to current row
-                        evidence_columns = [evidence_source_value, ' '.join(evidence_values), '; '.join(tag_values)]
+                        evidence_columns = [evidence_source_value, '; '.join(evidence_values), '; '.join(tag_values)]
                         tsv_content += '\t'.join(row_data) + '\t' + '\t'.join(evidence_columns) + '\n'
 
             # handle case where specimen data is not available
@@ -319,10 +321,10 @@ def json_to_tsv(source: str, target: str) -> None:
                     if add_comp_evidence_values_flag:
                         for evidence_text in evidence_source['evidence_list']:
                             evidence_values.append(evidence_text['evidence'])
-                        seen_evidence.add(' '.join(evidence_values))
+                        seen_evidence.add('; '.join(evidence_values))
                     
                     add_comp_evidence_values_flag = False
-                    evidence_columns = [evidence_source_value, ' '.join(evidence_values), '; '.join(tag_values)]
+                    evidence_columns = [evidence_source_value, '; '.join(evidence_values), '; '.join(tag_values)]
                     tsv_content += '\t'.join(row_data) + '\t' + '\t'.join(evidence_columns) + '\n'
                 
                 # re-initialize evidence column values to empty strings
@@ -350,11 +352,11 @@ def json_to_tsv(source: str, target: str) -> None:
                     if add_top_level_evidence_values_flag:
                         for evidence_text in evidence_source['evidence_list']:
                             evidence_values.append(evidence_text['evidence'])
-                        if ' '.join(evidence_values) in seen_evidence:
+                        if '; '.join(evidence_values) in seen_evidence:
                             continue
                     
                     add_top_level_evidence_values_flag = False
-                    evidence_columns = [evidence_source_value, ' '.join(evidence_values), '; '.join(tag_values)]
+                    evidence_columns = [evidence_source_value, '; '.join(evidence_values), '; '.join(tag_values)]
                     tsv_content += '\t'.join(row_data) + '\t' + '\t'.join(evidence_columns) + '\n'
             
     with open(target, 'w') as f:
@@ -371,47 +373,231 @@ def tsv_to_json(source: str, target: str) -> None:
         Filepath to the output file of type JSON. 
     '''
 
+    comp_singular_evidence_fields = {'biomarker', 'assessed_biomarker_entity', 'assessed_biomarker_entity_id', 'assessed_entity_type'}
+    top_level_evidence_fields = {'condition', 'exposure_agent', 'best_biomarker_role'}
+
     # flag for first row to check column headers are correct
     header_check = False
 
     with open(source, 'r') as f:
         
-        data = csv.reader(f, delimiter = '\t', quotechar = '"')
+        data = csv.DictReader(f, delimiter = '\t', quotechar = '"')
         # list to hold converted data 
         result_data = []
-    
+        # dictionary to hold the biomarker id's and their corresponding entry indices
+        id_map = {}
+        idx = 0
+
         # iterate through tsv data
         for row in data:
-
-            # top level structure
-            biomarker_entry = {
-                'biomarker_id': '',
-                'biomarker_component': [],
-                'best_biomarker_role': '',
-                'condition': None,
-                'exposure_agent': None,
-                'evidence_source': [],
-                'citation': []
-            }
             
             # make sure file headers are correct
             if not header_check:
-                for header in row:
+                for header in list(row.keys()):
                     if header not in TSV_HEADERS:
-                        logging.error(f'Incorrect column headers in TSV file: {row}')
+                        logging.error(f'Incorrect TSV column found: {header}')
                 header_check = True
-            # start conversion
+
+            comp_object_evidence_fields = {'specimen': row['specimen_id'], 'loinc_code': row['loinc_code']}
+
+
+            # handle evidence 
+            tags = [tag.strip() for tag in row['tag'].split(';')]
+            comp_evidence_tags = []
+            comp_evidence_found = False
+            top_evidence_tags = []
+            top_evidence_found = False
+            for tag in tags:
+                if tag in comp_singular_evidence_fields:
+                    comp_evidence_tags.append(
+                        {
+                            'tag': tag
+                        }
+                    )
+                    comp_evidence_found = True
+                if tag.split(':')[0].lower() in set(comp_object_evidence_fields.keys()):
+                    comp_evidence_tags.append(
+                        {
+                            'tag': f"{tag.split(':')[0].lower()}:{comp_object_evidence_fields[tag.split(':')[0].lower()]}"
+                        }
+                    )
+                    comp_evidence_found = True
+                if tag in top_level_evidence_fields:
+                    top_evidence_tags.append(
+                        {
+                            'tag': tag
+                        }
+                    )
+                    top_evidence_found = True
+            if comp_evidence_found:
+                # if the evidence database is in the url map then build the evidence url
+                if row['evidence_source'].split(':')[0].lower() in set(URL_MAP.keys()):
+                    comp_evidence_url = f"{URL_MAP[row['evidence_source'].split(':')[0].lower()]}{row['evidence_source'].split(':')[1]}"
+                else:
+                    logging.info(f"Couldn\'t build component level evidence URI:\n\tbiomarker_id: {row['biomarker_id']}\n\tevidence source: {row['evidence_source']}")
+                    comp_evidence_url = None 
+                comp_evidence_source = [{
+                    'evidence_id': row['evidence_source'].split(':')[1],
+                    'database': row['evidence_source'].split(':')[0],
+                    'url': comp_evidence_url,
+                    'evidence_list': [{'evidence': evidence_txt} for evidence_txt in row['evidence'].split('; ')],
+                    'tags': comp_evidence_tags
+                }]
             else:
-                # start building biomarker component
-                biomarker_component = {
-                    'biomarker': row['biomarker'],
-                    'assessed_biomarker_entity': {
-                        'recommended_name': row['assessed_biomarker_entity'],
-                        'synonyms': []
-                    },
-                    'assessed_biomarker_entity_id': row['assessed_biomarker_entity_id'],
-                    'assessed_entity_type': row['assessed_entity_type']
+                comp_evidence_source = []
+            if top_evidence_found:
+                # if the evidence database is in the url map then build the evidence url
+                if row['evidence_source'].split(':')[0].lower() in set(URL_MAP.keys()):
+                    top_evidence_url = f"{URL_MAP[row['evidence_source'].split(':')[0].lower()]}{row['evidence_source'].split(':')[1]}"
+                else:
+                    logging.info(f"Couldn\'t build top level evidence URI:\n\tbiomarker_id: {row['biomarker_id']}\n\tevidence source: {row['evidence_source']}")
+                    top_evidence_url = None 
+                top_evidence_source = [{
+                    'evidence_id': row['evidence_source'].split(':')[1],
+                    'database': row['evidence_source'].split(':')[0],
+                    'url': top_evidence_url,
+                    'evidence_list': [{'evidence': evidence_txt} for evidence_txt in row['evidence'].split('; ')],
+                    'tags': top_evidence_tags
+                }]
+            else:
+                top_evidence_source = []
+
+            # start building biomarker component
+            biomarker_component = {
+                'biomarker': row['biomarker'],
+                'assessed_biomarker_entity': {
+                    'recommended_name': row['assessed_biomarker_entity'],
+                    'synonyms': []
+                },
+                'assessed_biomarker_entity_id': row['assessed_biomarker_entity_id'],
+                'assessed_entity_type': row['assessed_entity_type']
+            }
+
+            # build specimen entry
+            if row['specimen'] or row['loinc_code']:
+                # if the specimen name space is in the url map then build the specimen url
+                if row['specimen_id'].split(':')[0].lower() in set(URL_MAP.keys()):
+                    specimen_url = f"{URL_MAP[row['specimen_id'].split(':')[0].lower()]}{row['specimen_id'].split(':')[1]}"
+                else:
+                    logging.info(f"Couldn\'t build specimen URI:\n\tbiomarker_id: {row['biomarker_id']}\n\tspecimen: {row['specimen_id']}")
+                    specimen_url = None
+                specimen = {
+                    'name': row['specimen'],
+                    'specimen_id': row['specimen_id'],
+                    'name_space': row['specimen_id'].split(':')[0],
+                    'url': specimen_url,
+                    'loinc_code': row['loinc_code']
                 }
+                biomarker_component['specimen'] = [specimen]
+                biomarker_component['evidence_source'] = comp_evidence_source
+            else:
+                biomarker_component['specimen'] = None
+                biomarker_component['evidence_source'] = comp_evidence_source
+
+            # build condition entry if applicable
+            if row['condition']:
+                # get full condition resource name is available
+                if row['condition_id'].split(':')[0].lower() in set(NAME_SPACE_MAP.keys()):
+                    condition_resource = NAME_SPACE_MAP[row['condition_id'].split(':')[0].lower()].title()
+                else:
+                    logging.info(f"Didn\'t find a full condition resource name:\n\tbiomarker_id: {row['biomarker_id']}\n\tcondition: {row['condition_id']}")
+                    condition_resource = row['condition_id'].split(':')[0]
+                # if the condition name space is in the url map then build the condition url
+                if row['condition_id'].split(':')[0].lower() in set(URL_MAP.keys()):
+                    condition_url = f"{URL_MAP[row['condition_id'].split(':')[0].lower()]}{row['condition_id'].split(':')[1]}"
+                else:
+                    logging.info(f"Couldn\'t build condition resource URI:\n\tbiomarker_id: {row['biomarker_id']}\n\tcondition: {row['condition_id']}")
+                    condition_url = None
+                condition = {
+                    'condition_id': row['condition_id'],
+                    'recommended_name': {
+                        'condition_id': row['condition_id'],
+                        'name': row['condition'],
+                        'description': None,
+                        'resource': condition_resource,
+                        "url": condition_url,
+                        'synonyms': []
+                    }
+                }
+            else:
+                condition = None
+            
+            # build top level entry
+            biomarker_entry = {
+                'biomarker_id': row['biomarker_id'],
+                'biomarker_component': [biomarker_component],
+                'best_biomarker_role': row['best_biomarker_role'],
+                'condition': condition,
+                'exposure_agent': None,
+                'evidence_source': top_evidence_source,
+                'citation': []
+            }
+
+            # add full entry if biomarker id not seen yet 
+            if row['biomarker_id'] not in set(id_map.keys()):
+                
+                # add to id map and increment index 
+                id_map[row['biomarker_id']] = idx 
+                idx += 1
+                # add entry to result data 
+                result_data.append(biomarker_entry)
+
+            # handle adding to existing component 
+            else: 
+
+                entry_idx = id_map[row['biomarker_id']]
+                existing_entry = result_data[entry_idx]
+                # check if a new component element should be added or existing component should be updated
+                component_to_update = -1
+                for component_idx, existing_component in enumerate(existing_entry['biomarker_component']):
+                    biomarker_condition = (biomarker_component['biomarker'] == existing_component['biomarker'])
+                    as_biomarker_en_condition = (biomarker_component['assessed_biomarker_entity']['recommended_name'] == existing_component['assessed_biomarker_entity']['recommended_name'])
+                    as_biomarker_en_id_condition = (biomarker_component['assessed_biomarker_entity_id'] == existing_component['assessed_biomarker_entity_id'])
+                    as_entity_type = (biomarker_component['assessed_entity_type'] == existing_component['assessed_entity_type'])
+                    component_conditional = (biomarker_condition and as_biomarker_en_condition and as_biomarker_en_id_condition and as_entity_type)
+                    # if the core component information matches then capture component entry index to add to
+                    if component_conditional:
+                        component_to_update = component_idx
+                        break 
+
+                # new component entry should be added
+                if component_to_update == -1:
+                    result_data[entry_idx]['biomarker_component'].append(biomarker_component)
+                # update the matching existing component entry found
+                else: 
+                    # add new specimen entry if applicable 
+                    if specimen != None:
+                        add_specimen = True
+                        for existing_specimen in existing_entry['biomarker_component'][component_to_update]['specimen']:
+                            specimen_condition = (specimen['name'] == existing_specimen['name'])
+                            loinc_condition = (specimen['loinc_code'] == existing_specimen['loinc_code'])
+                            if specimen_condition and loinc_condition:
+                                add_specimen = False
+                                break
+                        if add_specimen:
+                            result_data[entry_idx]['biomarker_component'][component_to_update]['specimen'].append(specimen)
+                    # add new evidence source entry if applicable 
+                    if comp_evidence_source != []:
+                        append_comp_evidence = True
+                        for existing_comp_evidence in existing_entry['biomarker_component'][component_to_update]['evidence_source']:
+                            if comp_evidence_source == existing_comp_evidence:
+                                append_comp_evidence = False
+                                break 
+                        if append_comp_evidence:
+                            result_data[entry_idx]['biomarker_component'][component_to_update]['evidence_source'].append(comp_evidence_source[0])
+
+                # handle adding to top level evidence if applicable
+                if top_evidence_source != []:
+                    append_top_evidence = True
+                    for existing_top_evidence in existing_entry['evidence_source']:
+                        if top_evidence_source == existing_top_evidence:
+                            append_top_evidence = False
+                            break
+                    if append_top_evidence:
+                        result_data[entry_idx]['evidence_source'].append(top_evidence_source[0])
+        
+        with open(target, 'w') as f:
+            json.dump(result_data, f)
 
 def setup_logging(log_path: str) -> None:
     ''' Configures the logger. 
@@ -427,11 +613,15 @@ def setup_logging(log_path: str) -> None:
 def main() -> None: 
 
     global _version 
+    global URL_MAP
+    global NAME_SPACE_MAP
     # grab version number 
     with open('../conf.json', 'r') as f:
         config = json.load(f) 
         _version = config['version']
         log_path = config[_CONF_KEY]['log_path']
+        URL_MAP = config['url_map']
+        NAME_SPACE_MAP = config['name_space_map']
     
     # make sure directory to dump logs in exists
     validate_filepath(os.path.split(log_path)[0], 'output')
