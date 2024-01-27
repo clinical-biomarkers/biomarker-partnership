ead7854794b20b0ab73c83f1f62d5731b3bcc126
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-01-27 18:53:09 -0500

    consistency cleanup

--- src/data_conversion/api_calls.py
+++ src/data_conversion/api_calls.py
@@ -2,13 +2,11 @@
 '''
 
 from pymed import PubMed
-import logging
 import requests 
 import re 
 from dotenv import load_dotenv
 import os
 from time import sleep
-import json
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import ParseError
 import misc_functions as misc_fns
@@ -49,8 +47,7 @@ def get_doid_data(doid_id: str, max_retries: int = 3, timeout: int = 5) -> dict:
 
             # handle errors
             if response.status_code != 200:
-                logging.error(f'Error during DOID API call for id {doid_id}:\n\tStatus Code: {doid_data.status_code}\n\tReturn Data: {doid_data.text}')
-                print(f'Error during DOID API call for id {doid_id}:\n\tStatus Code: {doid_data.status_code}\n\tReturn Data: {doid_data.text}')
+                misc_fns.print_and_log(f'Error during DOID API call for id \'{doid_id}\':\n\tStatus Code: \'{response.status_code}\'\n\tReturn Data: {response.text}', 'error')
                 return None
             
             # if no return error, continue to processing
@@ -70,17 +67,15 @@ def get_doid_data(doid_id: str, max_retries: int = 3, timeout: int = 5) -> dict:
             misc_fns.write_json('../../mapping_data/doid_map.json', doid_map)
             return return_data
         except (requests.ConnectionError, requests.Timeout, requests.HTTPError) as e:
-            logging.warning(f'Warning: Failed to connect to DOID API on attempt {attempt + 1}. Retrying...')
-            print(f'Warning: Failed to connect to DOID API. Retrying...')
+            misc_fns.print_and_log(f'Warning: Failed to connect to DOID API on attempt {attempt + 1}. Retrying...', 'warning')
             attempt += 1
             sleep(1)
             continue
         except Exception as e:
-            logging.error(f'Unexpected error while fetching DOID data for DOID ID {doid_id}:\n\t{e}')
-            print(f'Unexpected error while fetching DOID data for DOID ID {doid_id}:\n\t{e}')
+            misc_fns.print_and_log(f'Unexpected error while fetching DOID data for DOID ID {doid_id}:\n\t{e}', 'error')
             return None
     
-    logging.error(f'Failed to retrive DOID data after {max_retries} attempts.')
+    misc_fns.print_and_log(f'Failed to retrive DOID data after {max_retries} attempts.', 'error')
     return None
 
 def get_pubmed_data(pubmed_id: str) -> tuple:
@@ -110,11 +105,10 @@ def get_pubmed_data(pubmed_id: str) -> tuple:
     try:
         api_key = os.getenv('API_KEY')
     except Exception as e:
-        logging.warning(f'Warning: Failed to find API_KEY environment variable. Consider adding one.')
+        misc_fns.print_and_log(f'Warning: Failed to find API_KEY environment variable. Consider adding one.', 'warning')
         api_key = None
     if email is None:
-        logging.error(f'Error: Failed to find EMAIL environment variable. Check .env file.')
-        print(f'Error: Failed to find EMAIL environment variable. Check .env file. Skipping PubMed API calls...')
+        misc_fns.print_and_log(f'Error: Failed to find EMAIL environment variable. Check .env file. Skipping PubMed API calls...', 'warning')
         return 0, None 
 
     # query PubMed for the given PubMed ID
@@ -126,16 +120,13 @@ def get_pubmed_data(pubmed_id: str) -> tuple:
     try:
         article = next(articles)
     except StopIteration:
-        logging.error(f'Error: No articles found for PubMed ID {pubmed_id}')
-        print(f'Error: No articles found for PubMed ID {pubmed_id}')
+        misc_fns.print_and_log(f'Error: No articles found for PubMed ID \'{pubmed_id}\'', 'error')
         return 1, None
     except ParseError as e:
-        logging.error(f'XML Parsing Error: Failed to parse PubMed data for PubMed ID {pubmed_id}:\n\t{e}')
-        print(f'XML Parsing Error: Failed to parse PubMed data for PubMed ID {pubmed_id}:\n\t{e}')
+        misc_fns.print_and_log(f'XML Parsing Error: Failed to parse PubMed data for PubMed ID \'{pubmed_id}\':\n\t{e}', 'error')
         return 1, None
     except Exception as e:
-        logging.error(f'Unexpected error while fetching PubMed data for PubMed ID {pubmed_id}:\n\t{e}')
-        print(f'Unexpected error while fetching PubMed data for PubMed ID {pubmed_id}:\n\t{e}')
+        misc_fns.print_and_log(f'Unexpected error while fetching PubMed data for PubMed ID \'{pubmed_id}\':\n\t{e}', 'error')
         return 1, None
 
     # parse return data
@@ -145,8 +136,7 @@ def get_pubmed_data(pubmed_id: str) -> tuple:
         authors = ', '.join([f"{author['lastname']} {author['initials']}" for author in article.authors])
         publication_date = str(article.publication_date)
     except Exception as e:
-        logging.error(f'Error: Failed to parse PubMed data for PubMed ID {pubmed_id}:\n\tReturn JSON: {article}\n\t{e}')
-        print(f'Error: Failed to parse PubMed data for PubMed ID {pubmed_id}:\n\tReturn JSON: {article}\n\t{e}')
+        misc_fns.print_and_log(f'Error: Failed to parse PubMed data for PubMed ID \'{pubmed_id}\':\n\tReturn JSON: {article}\n\t{e}', 'error')
         return 1, None
     
     return_data = {
@@ -181,16 +171,15 @@ def get_uniprot_data(uniprot_id: str, assessed_entity_type: str) -> tuple:
     if uniprot_id in uniprot_map:
         return 0, uniprot_map[uniprot_id]
     
-    uniprot_data = requests.get(UNIPROT_API_ENDPOINT + uniprot_id)
+    response = requests.get(UNIPROT_API_ENDPOINT + uniprot_id)
 
     # handle errors
-    if uniprot_data.status_code != 200:
-        logging.error(f'Error during UniProt API call for id {uniprot_id}:\n\tStatus Code: {uniprot_data.status_code}\n\tReturn Data: {uniprot_data.text}')
-        print(f'Error during UniProt API call for id {uniprot_id}:\n\tStatus Code: {uniprot_data.status_code}\n\tReturn Data: {uniprot_data.text}')
+    if response.status_code != 200:
+        misc_fns.print_and_log(f'Error during UniProt API call for id \'{uniprot_id}\':\n\tStatus Code: \'{response.status_code}\'\n\tReturn Data: {response.text}', 'error')
         return 1, None
     
     # get protein name and synonyms
-    uniprot_data = uniprot_data.json()['protein']
+    uniprot_data = response.json()['protein']
     synonyms = []
     for recommended_short_name in uniprot_data.get('recommendedName', {}).get('shortName', []):
         synonyms.append(recommended_short_name['value'])
@@ -237,8 +226,7 @@ def get_chebi_data(chebi_id: str, max_retries: int = 3, timeout: int = 5) -> tup
         try:
             response = requests.get(CHEBI_API_ENDPOINT + chebi_id, timeout = timeout)
             if response.status_code != 200:
-                logging.error(f'Error during ChEBI API call for id {chebi_id}:\n\tStatus Code: {response.status_code}\n\tReturn Data: {response.text}')
-                print(f'Error during ChEBI API call for id {chebi_id}:\n\tStatus Code: {response.status_code}\n\tReturn Data: {response.text}')
+                misc_fns.print_and_log(f'Error during ChEBI API call for id \'{chebi_id}\':\n\tStatus Code: \'{response.status_code}\'\n\tReturn Data: {response.text}', 'error')
                 return 1, None
             root = ET.fromstring(response.content)
             chebi_name_element = root.find('.//chebi:chebiAsciiName', ns)
@@ -254,17 +242,15 @@ def get_chebi_data(chebi_id: str, max_retries: int = 3, timeout: int = 5) -> tup
             return 1, return_data
 
         except (requests.ConnectionError, requests.Timeout, requests.HTTPError) as e:
-            logging.warning(f'Warning: Failed to connect to ChEBI API on attempt {attempt + 1}. Retrying...')
-            print(f'Warning: Failed to connect to ChEBI API. Retrying...')
+            misc_fns.print_and_log(f'Warning: Failed to connect to ChEBI API on attempt {attempt + 1}. Retrying...', 'warning')
             attempt += 1
             sleep(1)
             continue
         except Exception as e:
-            logging.error(f'Unexpected error while fetching ChEBI data for ChEBI ID {chebi_id}:\n\t{e}')
-            print(f'Unexpected error while fetching ChEBI data for ChEBI ID {chebi_id}:\n\t{e}')
+            misc_fns.print_and_log(f'Unexpected error while fetching ChEBI data for ChEBI ID \'{chebi_id}\':\n\t{e}', 'error')
             return 1, None
     
-    logging.error(f'Failed to retrive ChEBI data after {max_retries} attempts.')
+    misc_fns.print_and_log(f'Failed to retrive ChEBI data for ChEBI ID \'{chebi_id}\' after {max_retries} attempts.', 'error')
     return 1, None
 
 def get_co_data(co_id: str, max_retries: int = 3, timeout: int = 5) -> tuple:
@@ -300,8 +286,7 @@ def get_co_data(co_id: str, max_retries: int = 3, timeout: int = 5) -> tuple:
 
             # handle errors 
             if response.status_code != 200:
-                logging.error(f'Error during Cell Ontology API call for id {co_id}:\n\tStatus Code: {response.status_code}\n\tReturn Data: {response.text}')
-                print(f'Error during Cell Ontology API call for id {co_id}:\n\tStatus Code: {response.status_code}\n\tReturn Data: {response.text}')
+                misc_fns.print_and_log(f'Error during Cell Ontology API call for ID \'{co_id}\':\n\tStatus Code: \'{response.status_code}\'\n\tReturn Data: {response.text}', 'error')
                 return 1, None
             
             # if no return error, continue to processing
@@ -318,17 +303,15 @@ def get_co_data(co_id: str, max_retries: int = 3, timeout: int = 5) -> tuple:
             misc_fns.write_json('../../mapping_data/co_map.json', co_map)
             return return_data
         except (requests.ConnectionError, requests.Timeout, requests.HTTPError) as e:
-            logging.warning(f'Warning: Failed to connect to Cell Ontology API on attempt {attempt + 1}. Retrying...')
-            print(f'Warning: Failed to connect to Cell Ontology API. Retrying...')
+            misc_fns.print_and_log(f'Warning: Failed to connect to Cell Ontology API on attempt {attempt + 1} for ID \'{co_id}\'. Retrying...', 'warning')
             attempt += 1
             sleep(1)
             continue
         except Exception as e:
-            logging.error(f'Unexpected error while fetching Cell Ontology data for Cell Ontology ID {co_id}:\n\t{e}')
-            print(f'Unexpected error while fetching Cell Ontology data for Cell Ontology ID {co_id}:\n\t{e}')
+            misc_fns.print_and_log(f'Unexpected error while fetching Cell Ontology data for Cell Ontology ID \'{co_id}\':\n\t{e}', 'error')
             return 1, None
     
-    logging.error(f'Failed to retrive Cell Ontology data after {max_retries} attempts.')
+    misc_fns.print_and_log(f'Failed to retrive Cell Ontology data for CO ID \'{co_id}\' after {max_retries} attempts.', 'error')
     return 1, None
 
 def get_hgnc_data(hgnc_id: str, max_retries: int = 3, timeout: int = 5) -> tuple:
@@ -388,6 +371,6 @@ def get_hgnc_data(hgnc_id: str, max_retries: int = 3, timeout: int = 5) -> tuple
             misc_fns.print_and_log(f'Unexpected error while fetching HGNC data for HGNC ID \'{hgnc_id}\':\n\t{e}', 'error')
             return 1, None
     
-    misc_fns.print_and_log(f'Failed to retrive HGNC data after {max_retries} attempts.', 'error')
+    misc_fns.print_and_log(f'Failed to retrive HGNC data for HGNC ID \'{hgnc_id}\' after {max_retries} attempts.', 'error')
     return 1, None
 
