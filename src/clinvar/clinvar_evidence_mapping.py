126744d93a61284bb395376561a1634b20bc13a7
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-04-22 11:48:16 -0400

    maps clinvar citation and evidence

--- src/clinvar/clinvar_evidence_mapping.py
+++ src/clinvar/clinvar_evidence_mapping.py
@@ -5,7 +5,6 @@ import json
 from xml.etree.ElementTree import ParseError
 import ijson  # type: ignore
 import os
-import sys
 from pymed import PubMed  # type: ignore
 from pymed.api import PubMedBookArticle  # type: ignore
 from dotenv import load_dotenv
@@ -15,9 +14,10 @@ citation_map_path = (
     "../../home/data/processed_data/GlyGen/clinvar/clinvar_citations_disease.json"
 )
 clinvar_json = "../../home/data/finalized_data/clinvar.json"
-output_json = "../../home/data/finalized_data/clinvar_2.json"
+output_json = "../../home/data/finalized_data/clinvar/clinvar-cancer-biomarkers-{}.json"
 pubmed_link = "https://pubmed.ncbi.nlm.nih.gov/{}"
 pubmed_map_path = "../../mapping_data/pubmed_map.json"
+pubmed_map = json.load(open(pubmed_map_path, "r"))
 component_tags = [
     "biomarker",
     "assessed_biomarker_entity",
@@ -30,15 +30,13 @@ with open(citation_map_path, "r") as f:
 
 
 def main():
-    chunk_size = 5_000
+    chunk_size = 10_000
     buffer = []
+    curr_file = 1
 
     with open(clinvar_json, "r") as f:
         array_entries = ijson.items(f, "item")
 
-        out_file = open(output_json, "w")
-        out_file.write("[\n")
-        first_record = True
         api_call_counter = 0
         for idx, item in enumerate(array_entries):
             print(f"On item: {idx}")
@@ -46,7 +44,11 @@ def main():
                 "assessed_biomarker_entity_id"
             ]
             condition_id = item["condition"]["id"]
-            clinvar_evidence_sources = [evidence for evidence in item["evidence_source"] if evidence["database"] == "Clinvar"]
+            clinvar_evidence_sources = [
+                evidence
+                for evidence in item["evidence_source"]
+                if evidence["database"] == "Clinvar"
+            ]
             if in_citation_map(assessed_biomarker_entity_id, condition_id):
                 for pmid in citation_map[assessed_biomarker_entity_id][condition_id]:
                     item["biomarker_component"][0]["evidence_source"].append(
@@ -67,20 +69,18 @@ def main():
                         api_call_counter = 0
             buffer.append(item)
             if len(buffer) >= chunk_size:
-                if not first_record:
-                    out_file.write(",\n")
-                json.dump(buffer, out_file, indent=2)
+                print("dumping chunk...")
+                with open(output_json.format(curr_file), "w") as out_file:
+                    json.dump(buffer, out_file, indent=2)
+                with open(pubmed_map_path, "w") as out:
+                    json.dump(pubmed_map, out, indent=4)
                 buffer = []
-                first_record = False
-                # TODO
-                sys.exit(0)
+                curr_file += 1
         if buffer:
-            if not first_record:
-                out_file.write(",\n")
-            json.dump(buffer, out_file, indent=2)
-        out_file.write("\n]")
-
-    out_file.close()
+            with open(output_json.format(curr_file), "w") as out_file:
+                json.dump(buffer, out_file, indent=2)
+            with open(pubmed_map_path, "w") as out:
+                json.dump(pubmed_map, out, indent=4)
 
 
 def generate_evidence_source(tags: list[str], pmid: str) -> dict:
@@ -111,11 +111,11 @@ def generate_citation_entry(
     api_call = citation_tuple[0]
     citation_data = citation_tuple[1]
     return_data = {
-        "citation_title": citation_data["title"],
+        "title": citation_data["title"],
         "journal": citation_data["journal"],
         "authors": citation_data["authors"],
         "date": citation_data["publication_date"],
-        "evidence_source": [],
+        "evidence": [],
         "reference": [{"id": pmid, "type": "Pubmed", "url": pubmed_link.format(pmid)}],
     }
     if evidence_source is not None:
@@ -133,10 +133,11 @@ def generate_citation_entry(
     return api_call, return_data
 
 
-def get_pubmed_data(pmid: str) -> tuple | None:
+def get_pubmed_data(pmid: str, max_retries: int = 3, retry_delay=5) -> tuple | None:
+
+    global pubmed_map
+
     pmid = pmid.strip()
-    with open(pubmed_map_path, "r") as f:
-        pubmed_map = json.load(f)
     if pmid in pubmed_map:
         return 0, pubmed_map[pmid]
 
@@ -144,45 +145,42 @@ def get_pubmed_data(pmid: str) -> tuple | None:
     email = os.getenv("EMAIL")
     api_key = os.getenv("API_KEY")
     pubmed = PubMed(tool="CFDE Biomarker-Partnership Citation Retriever", email=email)  # type: ignore
-    pubmed.parameters.update({"api_key": api_key})  # type: ignore
-    query = f"PMID: {pmid}"
-    articles = pubmed.query(query)
-    try:
-        article = next(articles)
-    except StopIteration as e:
-        print(f"StopIteration for PMID: {pmid}.\n{e}")
-        return 1, None
-    except ParseError as e:
-        print(f"ParseError for PMID: {pmid}.\n{e}")
-        return 1, None
-    except Exception as e:
-        print(f"Exception for PMID: {pmid}.\n{e}")
-        return 1, None
 
-    try:
-        title = article.title
-        if isinstance(article, PubMedBookArticle):
-            journal = f"Book: {article.title}"
-        else:
-            journal = article.journal
-        authors = ", ".join(
-            [f"{author['lastname']} {author['initials']}" for author in article.authors]
-        )
-        publication_date = str(article.publication_date)
-    except Exception as e:
-        print(f"Exception parsing artcile for PMID: {pmid}.\n{e}")
-        return 1, None
+    for attempt in range(max_retries):
+        try:
+            pubmed.parameters.update({"api_key": api_key})  # type: ignore
+            query = f"PMID: {pmid}"
+            articles = pubmed.query(query)
+            article = next(articles)
 
-    return_data = {
-        "title": title,
-        "journal": journal,
-        "authors": authors,
-        "publication_date": publication_date,
-    }
-    pubmed_map[pmid] = return_data
-    with open(pubmed_map_path, "w") as out:
-        json.dump(pubmed_map, out, indent=4)
-    return 1, return_data
+            title = article.title
+            if isinstance(article, PubMedBookArticle):
+                journal = f"Book: {article.title}"
+            else:
+                journal = article.journal
+            authors = ", ".join(
+                [
+                    f"{author['lastname']} {author['initials']}"
+                    for author in article.authors
+                ]
+            )
+            publication_date = str(article.publication_date)
+
+            return_data = {
+                "title": title,
+                "journal": journal,
+                "authors": authors,
+                "publication_date": publication_date,
+            }
+            pubmed_map[pmid] = return_data
+            return 1, return_data
+
+        except (StopIteration, ParseError, Exception) as e:
+            print(f"Error for PMID: {pmid} on attempt {attempt + 1}.\n{e}")
+            time.sleep(retry_delay)
+
+    print(f"Failed to retrieve data after {max_retries} attempts.")
+    return 1, None
 
 
 if __name__ == "__main__":
